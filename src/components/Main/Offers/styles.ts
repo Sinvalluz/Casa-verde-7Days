4bf73e17c9b6b338929f98ec4ec6aa5c509b15ee
Sinval Luz <contato.sinvalluz@gmail.com>
2024-07-25 19:58:33 -0300

    adding images to offers via server

--- src/components/Main/Offers/styles.ts
+++ src/components/Main/Offers/styles.ts
@@ -1,73 +1,74 @@
-import styled from 'styled-components';
-import theme from '../../../styles/theme';
+import styled from "styled-components";
+import theme from "../../../styles/theme";
 
 export const Container = styled.section`
-	display: flex;
-	flex-direction: column;
-	align-items: center;
-	margin-top: 4.4rem;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-top: 4.4rem;
 `;
 
 export const Paragraph = styled.p`
-	font-size: 2.2rem;
-	opacity: 50%;
-	margin-bottom: 1.2rem;
+  font-size: 2.2rem;
+  opacity: 50%;
+  margin-bottom: 1.2rem;
 `;
 
 export const Title = styled.h2`
-	font-size: 8.2rem;
-	font-weight: ${theme.font.weigth.bold};
-	font-family: ${theme.font.family.elsie};
-	margin-bottom: 0.5rem;
+  font-size: 8.2rem;
+  font-weight: ${theme.font.weigth.bold};
+  font-family: ${theme.font.family.elsie};
+  margin-bottom: 0.5rem;
 
-	@media screen and (max-width: 346px) {
-		font-size: 5rem;
-	}
+  @media screen and (max-width: 346px) {
+    font-size: 5rem;
+  }
 `;
 
 export const Products = styled.div`
-	display: flex;
-	flex-wrap: wrap;
-	gap: 3rem;
-	width: 100%;
-	place-content: center;
+  display: flex;
+  flex-wrap: wrap;
+  gap: 3rem;
+  width: 100%;
+  place-content: center;
 `;
 
 export const Product = styled.div`
-	display: flex;
-	max-width: 380px;
-	box-shadow: 10px 10px 30px 0 rgba(0, 0, 0, 0.06);
+  display: flex;
+  max-width: 380px;
+  box-shadow: 10px 10px 30px 0 rgba(0, 0, 0, 0.06);
 `;
 
 export const Image = styled.img`
-	width: 60%;
+  width: 60%;
 
-	@media screen and (max-width: 425px) {
-		width: 40%;
-	}
+  @media screen and (max-width: 425px) {
+    width: 40%;
+  }
 `;
 
 export const ProductContent = styled.div`
-	padding: 2.7rem 0;
+  padding: 2.7rem 0;
 `;
 
 export const ProductTitle = styled.h3`
-	font-size: 3.2rem;
-	font-weight: ${theme.font.weigth.bold};
-	font-family: ${theme.font.family.elsie};
-	margin-bottom: 0.8rem;
+  font-size: 3.2rem;
+  font-weight: ${theme.font.weigth.bold};
+  font-family: ${theme.font.family.elsie};
+  margin-bottom: 0.8rem;
 `;
 
 export const Price = styled.p`
-	font-size: 1.6rem;
-	opacity: 50%;
-	margin-bottom: 2.4rem;
+  font-size: 1.6rem;
+  opacity: 50%;
+  margin-bottom: 2.4rem;
 `;
 
 export const Button = styled.button`
-	display: flex;
-	align-items: center;
-	color: ${theme.colors.butoon};
-	border: none;
-	background: transparent;
+  display: flex;
+  align-items: center;
+  color: ${theme.colors.butoon};
+  border: none;
+  background: transparent;
+  cursor: pointer;
 `;
